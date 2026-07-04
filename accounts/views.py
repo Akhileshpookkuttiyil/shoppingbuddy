de7fc675e29f5c7317ba293881ce09a05ff306c9
Akhileshpookkuttiyil <Akhileshpookkuttiyil@users.noreply.github.com>
2026-07-04 21:30:33 +0530

    refactor(auth): replace manual auth with Django forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,38 +1,45 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import User,auth
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.http import url_has_allowed_host_and_scheme
+from .forms import RegistrationForm
 
 def login(request):
-    if request.method=='POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        user=auth.authenticate(username=username,password=password)
-        if user is not None:
-            auth.login(request,user)
-            print('login successfull')
+    if request.user.is_authenticated:
+        return redirect('/')
+        
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            
+            # Prevent Open Redirect vulnerability
+            next_url = request.POST.get('next') or request.GET.get('next')
+            if next_url and url_has_allowed_host_and_scheme(
+                url=next_url,
+                allowed_hosts={request.get_host()},
+                require_https=request.is_secure()
+            ):
+                return redirect(next_url)
             return redirect('/')
-        else:
-            print('login failed')
-            return redirect('login')
-    return render(request,'login.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
 def register(request):
-    if request.method=='POST':
-        firstname=request.POST['firstname']
-        lastname=request.POST['lastname']
-        username=request.POST['username']
-        email=request.POST['email']
-        password=request.POST['password1']
-        confirm_password=request.POST['password2']
-        if password==confirm_password:
-            user=User.objects.create_user(first_name=firstname,last_name=lastname,username=username,email=email,password=password)
-            user.save()
-            print("new user")
+    if request.user.is_authenticated:
+        return redirect('/')
+        
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('login')
-        else:
-            return redirect('register')
-    return render(request,'register.html')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
 
 def logout(request):
-    auth.logout(request)
+    auth_logout(request)
     return redirect('/')
